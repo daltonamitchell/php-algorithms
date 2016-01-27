8ca5807584983cd1469bf07d6f55538715c74f38
Dalton A. Mitchell <daltonamitchell@live.com>
2016-01-27 18:22:17 +0000

    Exclude ruby

--- brackets_combinations.rb
+++
@@ -1,9 +0,0 @@
-def brackets(pairs, output='', open=0, close=0, arr = [])
-  arr << output if open == pairs and close == pairs      
-  brackets pairs, output+'(', open+1, close, arr if open<pairs
-  brackets pairs, output+')', open, close+1, arr if close<open
-  arr
-end
-
-#test
-p brackets(3) #["((()))", "(()())", "(())()", "()(())", "()()()"]

b234b27b3358ff1a5dd134f65814a5df38a66c63
Dalton A. Mitchell <daltonamitchell@live.com>
2016-05-22 11:43:27 -0700

    Remove old project files

--- src/autoload.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-// Require composers autoloader so we can use namespacing
-require __DIR__ . '/../vendor/autoload.php';
--- tests/ExampleTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class ExampleTest extends PHPUnit_Framework_TestCase
-{
-    public function test_is_true()
-    {
-        $this->assertEquals(true, true);
-    }
-}
